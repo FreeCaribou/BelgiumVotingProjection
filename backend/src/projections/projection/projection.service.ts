13ebc5bdc4aad43e1936b2d5d9b85e741189f33b
FreeCaribou <florian@gerardweb.eu>
2023-04-12 17:00:33 +0200

    POST 3

--- backend/src/projections/projection/projection.service.ts
+++ backend/src/projections/projection/projection.service.ts
@@ -47,13 +47,13 @@ export class ProjectionService {
   // TODO sync mode please
   async add(addProjectionDto: AddProjectionDto) {
     let projection = await this.projectionRepository.save(await this.projectionRepository.create(addProjectionDto));
-    for (let i = 0; i < addProjectionDto.parties.length; i++) {
-      await this.partyRepository.save(await this.partyRepository.create({
-        projection,
-        label: addProjectionDto.parties[i].label,
-        votes: addProjectionDto.parties[i].votes,
-      }));
-    }
+    // for (let i = 0; i < addProjectionDto.parties.length; i++) {
+    //   await this.partyRepository.save(await this.partyRepository.create({
+    //     projection,
+    //     label: addProjectionDto.parties[i].label,
+    //     votes: addProjectionDto.parties[i].votes,
+    //   }));
+    // }
     projection.date = new Date();
     return this.projectionRepository.save(projection);
   }
